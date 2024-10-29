cdea5e758abe4747d873c41c4c981b922f5bfaab
syk25 <syk2245@gmail.com>
2024-10-29 17:13:36 +0900

    as, attrs

--- src/App.js
+++ src/App.js
@@ -4,27 +4,19 @@ const Father = styled.div`
     display: flex;
 `;
 
-const Box = styled.div`
-    background-color: ${(props) => props.bgColor};
-    width: 100px;
-    height: 100px;
-`;
-
-const Circle = styled(Box)`
-    border-radius: 50px;
-`;
-
-const Text = styled.span`
-    color: white;
+const Input = styled.input.attrs({ required: true })`
+    background-color: tomato;
 `;
 
 function App() {
     return (
-        <Father>
-            <Box bgColor='tomato'>
-                <Text>Hello</Text>
-            </Box>
-            <Circle bgColor='teal' />
+        <Father as='header'>
+            <Input />
+            <Input />
+            <Input />
+            <Input />
+            <Input />
+            <Input />
         </Father>
     );
 }
