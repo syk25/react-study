c3b4b8e3f6710f7de90fa56666730e438b5225d2
syk25 <syk2245@gmail.com>
2024-10-29 18:49:24 +0900

    pseudo selectors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
     display: flex;
+    height: 100vh;
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
 `;
 
-const Input = styled.input.attrs({ required: true })`
+const rotationAnimation = keyframes`0%{
+    transform:rotate(0deg);
+    border-radius: 0px;
+} 50%{
+    border-radius: 100px;
+} 100%{
+    transform: rotate(360deg);
+    border-radius: 0px;
+}`;
+
+const Emoji = styled.span`
+    font-size: 36px;
+`;
+
+const Box = styled.div`
+    height: 200px;
+    width: 200px;
     background-color: tomato;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation: ${rotationAnimation} 1s linear infinite;
+    ${Emoji}:hover {
+        font-size: 98px;
+    }
 `;
 
 function App() {
     return (
-        <Father as='header'>
-            <Input />
-            <Input />
-            <Input />
-            <Input />
-            <Input />
-            <Input />
-        </Father>
+        <Wrapper>
+            <Box>
+                <Emoji>🥰</Emoji>
+            </Box>
+            <Emoji>🚀</Emoji>
+        </Wrapper>
     );
 }
 
